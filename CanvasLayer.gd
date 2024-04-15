004943ade688d8445e6102c74e45eff20762ae5a
Jalynaustin <jalynaustin05@gmail.com>
2024-04-15 14:38:28 -0400

    Added Resizing Cursor
    Resizes based on window. There is a bit of lag with the actually cursor. Added canvas layer over game scene for cursor.

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -1,19 +1,13 @@
 extends CanvasLayer
 
-var dialog_text = {} # The ELIZA text file that lists all the possible responses. I think.
-var current_text = [] # The text that will be currently shown in the dialog box.
+export(Texture) var empty_cursor = null
+export(Texture) var real_cursor = null
 
 func _ready():
-	var background: TextureRect = $Background
+	Input.set_custom_mouse_cursor(real_cursor, Input.CURSOR_ARROW)
 	# dialog_text = load_dialog_text() # doesn't work but we might need it
-	pass
 
-func _process(_delta):
-	pass
+func _process(delta):
+	$Sprite.global_position = $Sprite.get_global_mouse_position()
 
-func _on_user_input_text_text_submitted(_new_text):
-	pass
-
-func _on_quit_button_down():
-	get_tree().quit()
 
