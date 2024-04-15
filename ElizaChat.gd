474493459b85bfe21d04523efe672a4325b745f7
ku00789 <ku00789@georgiasouthern.edu>
2024-04-15 02:28:42 -0400

    Add Custom Text Effect for Typewrite
    Display Eliza responses as though it is being typed letter by letter.

--- ElizaChat.gd
+++ ElizaChat.gd
@@ -1,16 +1,11 @@
 extends RichTextLabel
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var max_lines = 30
 var user_name_text_color = "green"
 var user_request_text_color = "white"
 var eliza_name_text_color = "blue"
 var eliza_response_text_color = "white"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,14 +24,11 @@ func append_bbline(line):
 	
 func add_eliza_response(response):
 	var to_append = '[color='+eliza_name_text_color+']ELIZA:\t[/color]'
-	to_append += '[color='+eliza_response_text_color+']'+response+'[/color]'
+	to_append += '[color='+eliza_response_text_color+'][typewrite]'+response+'[/typewrite][/color]'
 	append_bbline(to_append)
-	
+
+
 func add_user_request(request):
 	var to_append = '[color='+user_name_text_color+']YOU:\t[/color]'
 	to_append += '[color='+user_request_text_color+']'+request+'[/color]'
 	append_bbline(to_append)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
