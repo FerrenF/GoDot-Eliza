3eb07228115d42b8553e3b8378b5f839ac057799
FerrenF <kfateweaver@gmail.com>
2024-04-17 12:29:58 -0400

    final touches on typewrite effect!

--- ElizaChat.gd
+++ ElizaChat.gd
@@ -11,24 +11,46 @@ var eliza_response_text_color = "white"
 func _ready():
 	pass # Replace with function body.
 
-func append_bbline(line):
+func append_bbline(line, typewrite=false, wrapper=[]):
 	var current_lines = get_node(".").bbcode_text
 	var li = PoolStringArray([]) if current_lines.empty() else current_lines.split('\n')
 	var current_count = len(li)
 	if current_count > max_lines:
 		li.remove(0)
+	
+	
+	# strip all typewriting effects.
+	for n in range(len(li)-1,0,-1):
+		var st = li[n]
+		var s = st.find('[typewrite]')
+		if s != -1:
+			st.erase(s,len('[typewrite]'))
+			s = st.find('[/typewrite]')
+			if s != -1:
+				st.erase(s,len('[/typewrite]'))
+		li[n] = st
+	
+	# Add one back if we need it
+	if typewrite:
+		line = '[typewrite]'+line+'[/typewrite]'
+		
+	# Apply a wrapper if we have one
+	if wrapper:
+		line = wrapper[0] + line;
+		if len(wrapper) > 1:
+			line += wrapper[1]
 		
 	li.push_back(line)
 	current_lines = li.join('\n')
 	get_node(".").bbcode_text = current_lines
 	
 func add_eliza_response(response):
-	var to_append = '[color='+eliza_name_text_color+']ELIZA:\t[/color]'
-	to_append += '[color='+eliza_response_text_color+'][typewrite]'+response+'[/typewrite][/color]'
-	append_bbline(to_append)
+	var to_append = '[color='+eliza_response_text_color+']'+response+'[/color]'
+	var wrapper = [ '[color='+eliza_name_text_color+']ELIZA:\t[/color]' ]
+	append_bbline(to_append, true, wrapper)
 
 
 func add_user_request(request):
-	var to_append = '[color='+user_name_text_color+']YOU:\t[/color]'
-	to_append += '[color='+user_request_text_color+']'+request+'[/color]'
-	append_bbline(to_append)
+	var to_append =  '[color='+user_request_text_color+']'+request+'[/color]'
+	var wrapper = ['[color='+user_name_text_color+']YOU:\t[/color]']
+	append_bbline(to_append, false, wrapper)
