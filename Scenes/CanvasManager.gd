29764128e167d6de9071e6b2849218ad0dc27a70
FerrenF <kfateweaver@gmail.com>
2024-04-17 11:08:56 -0400

    organization refactoring

--- Scenes/CanvasManager.gd
+++ Scenes/CanvasManager.gd
@@ -1,8 +1,7 @@
 extends CanvasLayer
 
 func _ready():
-	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	
 	# dialog_text = load_dialog_text() # doesn't work but we might need it
 
